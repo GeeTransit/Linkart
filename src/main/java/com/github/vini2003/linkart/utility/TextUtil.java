365f6749707692d9b99382fe70605c740ae8eaec
melontini <104443436+melontini@users.noreply.github.com>
2023-08-12 12:45:41 +0700

    TextUtil fix

--- src/main/java/com/github/vini2003/linkart/utility/TextUtil.java
+++ src/main/java/com/github/vini2003/linkart/utility/TextUtil.java
@@ -1,7 +1,9 @@
 package com.github.vini2003.linkart.utility;
 
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.math.BlockPos;
 
@@ -13,11 +15,11 @@ public class TextUtil {
     }
 
     public static MutableText literal(String value, Formatting formatting) {
-        return Text.literal(value).formatted(formatting);
+        return new LiteralText(value).formatted(formatting);
     }
 
     public static MutableText literal(Serializable value) {
-        return Text.literal(value.toString());
+        return new LiteralText(value.toString());
     }
 
     public static String blockPosAsString(BlockPos pos) {
@@ -25,10 +27,10 @@ public class TextUtil {
     }
 
     public static MutableText translatable(String key) {
-        return Text.translatable(key);
+        return new TranslatableText(key);
     }
 
     public static MutableText translatable(String key, Object... args) {
-        return Text.translatable(key, args);
+        return new TranslatableText(key, args);
     }
 }
